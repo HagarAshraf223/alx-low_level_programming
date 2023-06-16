e13ee953b37d3903f7e23317c799193f3c16ec2c
HagarAshraf223 <gooryashref@gmail.com>
2023-06-15 19:22:22 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdioh>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Description: A C program that prints with put function
  *
- * Return: Alawys 0 (success)
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
