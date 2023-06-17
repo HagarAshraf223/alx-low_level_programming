52628543d9ea82c6c0f4c3e93aad68cf88d77cd5
HagarAshraf223 <gooryashref@gmail.com>
2023-06-16 17:04:48 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
@@ -11,5 +12,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"):
-	return (0):
+		return (0):
 }
