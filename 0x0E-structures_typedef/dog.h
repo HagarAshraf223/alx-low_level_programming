a314b5790275d89edcc32396142a65ba204338d0
HagarAshraf223 <gooryashref@gmail.com>
2023-07-17 17:50:46 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - a dog struct
+ * struct dog - a dog's basic info
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
@@ -15,5 +15,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *str);
+int _strlen(char *s);
 
 #endif
